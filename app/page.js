a966ad42b8857af093eec439d5a9e5fa1102e836
soutovp <fernandoandradesouto@hotmail.com>
2023-08-22 12:03:06 -0300

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { container } from "./main.css.ts"
+import { container } from "./main.css"
 export default function Page() {
-	return <h1 className={container}>Hello, AEA.., Next.js!</h1>
+	return <h1 className={container}> Hello from next</h1 >
 }
