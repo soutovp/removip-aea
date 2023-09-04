c6dad7670a5c34e5688126858660ddd3aa6aa135
Mahdasah <headbanger.87@hotmail.com>
2023-09-04 00:07:10 -0300

    update

--- app/servicos/page.js
+++ app/servicos/page.js
@@ -2,44 +2,10 @@ import Atendimento from '../components/Atendimento'
 import Banner from '../components/Banner'
 import Aberto from '../components/Aberto'
 import servicosStyle from '../styles/pages/servicos/page.module.sass'
+import servicos from './servicos.json'
+
 export default function Page() {
-	const servicos = [
-		{
-			imagem: { img: 'servico_resgate-aeromedico', alt: 'Imagem de um helicoptero.' },
-			title: 'Transporte Aeromédico',
-			text: (<><p>Sistema de transporte aéro integrado com o terrestre.</p><p>Transferência intermunicipal, interestadual e internacional.</p></>)
-		},
-		{
-			imagem: { img: 'servico_remocao-ambulancia-particular', alt: 'Imagem de uma paramédica atrás de uma ambulância.' },
-			title: 'Remoção de Pacientes',
-			text: (<>
-				<p>Residência -&gt; Hospital</p>
-				<span>(internação/exame)</span>
-				<p>Hospital -&gt; Residência</p>
-				<span>(Alta hospitalar)</span>
-				<p>Residência -&gt; Clínica/consultório -&gt; Residência</p>
-				<span>(Exame)</span>
-			</>)
-		},
-		{
-			imagem: { img: 'servico_ambulancia-cobertura-medica', alt: 'Imagem de uma ambulância com três paramédicos na frente dela.' },
-			title: 'Cobertura médica de eventos',
-			text: (<><p>Cobertura médica e infraestrutura completa para qualquer evento, seja ele: esportivo, social, cultural, religioso ou corporativo.</p></>)
-		},
-		{
-			imagem: { img: 'servico_posto-medico-evento', alt: 'Imagem de uma sala de atendimento clínico.' },
-			title: 'Postos Médicos',
-			text: (<><p>Implantação e gestão de ambulatórios lacais. Segurança garantida onde for preciso</p></>)
-		},
-		{
-			imagem: { img: 'servico_locacao-aluguel-ambulancia', alt: 'Imagem de duas ambulâncias estacionadas.' },
-			title: 'Locação de Ambulâncias',
-			text: (<>
-				<p>Moderna frotas de ambulâncias.</p>
-				<p>Flexibilidade do período contratado.</p>
-			</>)
-		}
-	]
+
 	return (
 		<>
 			<Banner img={'cama'} alt={'Imagem de maca sendo removida de uma ambulância.'} />
@@ -47,15 +13,13 @@ export default function Page() {
 			<section className={servicosStyle.servicos}>
 				<h1>Conheça algumas de nossas soluções</h1>
 				<div>
-					{servicos.map(servico => {
-						return (<>
-							<article>
-								<img src={`/images/${servico.imagem.img}.png`} alt={servico.imagem.alt} />
-								<h2>{servico.title}</h2>
-								{servico.text}
-							</article>
-						</>)
-					})}
+					{servicos.map((servico) => (
+						<article key={servico.title}>
+							<img src={`/images/${servico.imagem.img}.png`} alt={servico.imagem.alt} />
+							<h2>{servico.title}</h2>
+							<div dangerouslySetInnerHTML={{ __html: servico.text }} />
+						</article>
+					))}
 				</div>
 			</section>
 			<Aberto className={servicosStyle.aberto} />
